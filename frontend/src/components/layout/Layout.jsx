698214b7968b4fec3f729db3a0f5e873550e88da
Dylan Bagnawe <dylanbagnawe@gmail.com>
2026-08-18 18:51:46 +0100

    Add FeedbackWidget to Layout component

--- frontend/src/components/layout/Layout.jsx
+++ frontend/src/components/layout/Layout.jsx
@@ -1,5 +1,6 @@
 import Navbar from "./Navbar";
 import Footer from "./Footer";
+import FeedbackWidget from "./FeedbackWidget";
 
 export default function Layout({ children }) {
   return (
@@ -7,6 +8,7 @@ export default function Layout({ children }) {
       <Navbar />
       <main className="flex-1">{children}</main>
       <Footer />
+      <FeedbackWidget />
     </div>
   );
 }
