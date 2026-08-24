1a05b5d0cb618db408d39fc40923e5bc4ae98f16
Dylan Bagnawe <dylanbagnawe@gmail.com>
2026-08-24 19:58:03 +0100

    Integrate useActivityHeartbeat hook in App component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,10 @@
 import { BrowserRouter } from "react-router-dom";
 import Layout from "./components/layout/Layout";
 import AnimatedRoutes from "./components/layout/AnimatedRoutes";
+import useActivityHeartbeat from "./hooks/useActivityHeartbeat";
 
 export default function App() {
+  useActivityHeartbeat();
   return (
     <BrowserRouter>
       <Layout>
